598ff16ffcd8502352b4d7e5cdc570c7169418e1
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-10-25 17:54:12 +0200

    [Setup] Ajoute React Testing Library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("peut exécuter du testing-library", () => {
+  const app = render(<App />);
+
+  expect(app.container).toHaveTextContent("Learn React");
 });
