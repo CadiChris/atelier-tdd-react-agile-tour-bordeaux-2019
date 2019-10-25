be6f792db796f63dd29cdc3227d2106886516198
Christophe Cadilhac <christophe.cadilhac@gmail.com>
2019-10-25 18:39:50 +0200

    [Ardoise] Implémente l'ardoise vierge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Ardoise</h1>
+      <div data-testid="participants">
+        <h3>Aucun participant</h3>
+      </div>
+
+      <div data-testid="depenses">
+        <h3>Aucune dépense</h3>
+      </div>
+
+      <div data-testid="creances">
+        <h3>Aucune créance</h3>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-it("peut exécuter du testing-library", () => {
-  const app = render(<App />);
+afterEach(cleanup);
 
-  expect(app.container).toHaveTextContent("Ardoise");
+describe("<App />", () => {
+  it("affiche une ardoise vierge", () => {
+    const app = render(<App />);
+
+    const participants = app.getByTestId("participants");
+    expect(participants).toHaveTextContent(
+      "Aucun participant"
+    );
+
+    const depenses = app.getByTestId("depenses");
+    expect(depenses).toHaveTextContent("Aucune dépense");
+
+    const creances = app.getByTestId("creances");
+    expect(creances).toHaveTextContent("Aucune créance");
+  });
 });
